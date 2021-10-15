5103e92cd2ee80a196a2aaeb500fc076a3e65dea
Raúl Monzon <rmonzon1998@gmail.com>
2021-10-14 23:35:49 -0600

    Update GUI.py
    Nueva GUI, sigue igual de fea pero ahora funciona mejor. ¿Eso ya cuenta como mejora, no?

--- Proyecto/GUI.py
+++ Proyecto/GUI.py
@@ -1,5 +1,6 @@
 from sys import int_info
 import tkinter as tk
+from tkinter import ttk
 import os
 from tkinter.constants import ANCHOR
 from tkinter.filedialog import askopenfilename, asksaveasfilename
@@ -16,7 +17,7 @@ def abrir():
     with open(filepath, "r") as input_file:
         text = input_file.read()
         txt_edit.insert(tk.END, text)
-    label_errors.config(text='')
+    label_ci.insert(tk.END, "")
 
 def guardar():
     filepath = asksaveasfilename(
@@ -28,7 +29,7 @@ def guardar():
     with open(filepath, "w") as output_file:
         text = txt_edit.get(1.0, tk.END)
         output_file.write(text)
-    label_errors.config(text='')
+    label_ci.insert(tk.END, "")
     
 
 def ejecutar():
@@ -36,31 +37,44 @@ def ejecutar():
     with open(filepath, "w") as output_file:
         text = txt_edit.get(1.0, tk.END)
         output_file.write(text)
-    errors = executeWalker(filepath)
+    errors, ic = executeWalker(filepath)
     lines = len(errors)
     errorsMsg = ''
     for i in range(lines):
         errorsMsg = errorsMsg + errors[i]+'\n'
     if (lines == 0): 
         errorsMsg = "No hay errores"
-    label_errors.config(text=errorsMsg)
+    label_ci.insert(tk.END, "\t\t\tErrores:\n"+errorsMsg+"\n\n\t\t\tCodigo intermedio:"+ic)
 
 #--------------------------GUI--------------------------
 userI = tk.Tk()
 userI.title("Proyecto de construcción de compiladores")
+
 userI.rowconfigure(0, minsize=800, weight=1)
-userI.columnconfigure(0, minsize=400, weight=1)
-userI.columnconfigure(1, minsize=400, weight=1)
-txt_edit = tk.Text(userI)
-fr_left_panel = tk.Frame(userI)
-btn_open = tk.Button(fr_left_panel, text="Abrir", command=abrir)
-btn_save = tk.Button(fr_left_panel, text="guardar", command=guardar)
-btn_run = tk.Button(fr_left_panel, text="Ejecutar", command=ejecutar)
-label_errors = tk.Label(fr_left_panel, text="")
-btn_open.grid(row=0, column=0, sticky="EW", padx=5)
-btn_save.grid(row=1, column=0, sticky="EW", padx=5)
-btn_run.grid(row=2, column=0, sticky="EW", padx=5)
-label_errors.grid(row=0, column=2, sticky="E", padx=5, pady=5)
-fr_left_panel.grid(row=0, column=0, sticky="NS")
-txt_edit.grid(row=0, column=1, sticky="NSEW")
+userI.columnconfigure(0)
+userI.columnconfigure(1)
+userI.columnconfigure(2)
+userI.columnconfigure(3)
+
+code_frame = ttk.Frame(userI)
+code_frame.rowconfigure(0, minsize=800, weight=1)
+code_frame.columnconfigure(0, minsize=150, weight=1)
+txt_edit = tk.Text(code_frame)
+txt_edit.grid(row=0, column=0, sticky="NSEW")
+code_frame.grid(column=0, row=0)
+
+ci_frame = ttk.Frame(userI)
+ci_frame.rowconfigure(0, minsize=800, weight=1)
+ci_frame.columnconfigure(0, weight=1)
+label_ci = tk.Text(ci_frame)
+label_ci.grid(row=0, column=0, sticky="NSEW", padx=5, pady=5)
+ci_frame.grid(column=1, row=0)
+
+buttons_frame = ttk.Frame(userI)
+buttons_frame.columnconfigure(0, weight=1)
+ttk.Button(buttons_frame, text= 'Abrir', command=abrir).grid(column=0, row=0)
+ttk.Button(buttons_frame, text='Guardar', command=guardar).grid(column=0, row=1)
+ttk.Button(buttons_frame, text= 'Ejecutar', command=ejecutar).grid(column=0, row=2)
+buttons_frame.grid(column=2, row=0)
+
 userI.mainloop()
